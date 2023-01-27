8ae603b88f56dd42144bdf05c4ef16ab1030ea60
Michael R. Taylor <michael@atomic.city>
2023-01-26 22:32:34 -0600

    Project created.

---
+++ ConfigSync/Config.cs
@@ -0,0 +1,26 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace ConfigSync;
+
+public class Config
+{
+	public class Application
+	{
+		public string ConfigLocation { get; set; }
+	}
+
+	public Dictionary<string, Application> Configs { get; set; }
+
+	public static Config ReadConfig(string yamlString)
+	{
+		using (var input = new StringReader(yamlString))
+		{
+			var deserializer = new DeserializerBuilder()
+				.WithNamingConvention(CamelCaseNamingConvention.Instance)
+				.Build();
+
+			return deserializer.Deserialize<Config>(input);
+		}
+	}
+}
---
+++ ConfigSync/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConfigSync;
+
+const string Document = @"
+configs:
+  alacritty:
+    configLocation: configDir/alacritty
+  helix-editor:
+    configLocation: configDir/helix
+";
+
+var config = Config.ReadConfig(Document);
+
+System.Console.WriteLine(config.Configs.First().Value.ConfigLocation);
