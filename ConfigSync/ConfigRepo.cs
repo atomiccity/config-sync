4ad214e1cef04af7f7fd9d99886d94a707ff8ce2
Michael R. Taylor <michael@atomic.city>
2023-01-28 01:37:24 -0600

    WIP on Backup and Restore functionality.

--- ConfigSync/ConfigRepo.cs
+++ ConfigSync/ConfigRepo.cs
@@ -3,22 +3,47 @@
 public class ConfigRepo
 {
 	private DirectoryInfo _baseDir;
-	private FileInfo _configFile;
-	private FileInfo _tokenFile;
 	private readonly Config _config;
+	private readonly OsConfig _osConfig;
 
 	public ConfigRepo(string baseDir, string configFile = "software.yaml", string tokenFile = "tokens.yaml")
 	{
 		_baseDir = new DirectoryInfo(baseDir);
-		_configFile = new FileInfo(configFile);
-		_tokenFile = new FileInfo(tokenFile);
-		var tokenMap = TokenMap.ReadConfig(_tokenFile.OpenText().ReadToEnd());
-		_config = Config.ReadConfig(_configFile.OpenText().ReadToEnd(), tokenMap);
+		var configFileInfo = new FileInfo(configFile);
+		var tokenFileInfo = new FileInfo(tokenFile);
+		var tokenMap = TokenMap.ReadConfig(tokenFileInfo.OpenText().ReadToEnd());
+		_config = Config.ReadConfig(configFileInfo.OpenText().ReadToEnd(), tokenMap);
+		_osConfig = new OsConfig(tokenMap);
 	}
 
 	public void Backup(string appName)
 	{
+		/*
+		val appConfig = getConfig().configs[appName] ?: error("Invalid application name")
+		val configRoot = File(appConfig.configLocation)
+		val osConfig = OsConfig()
 
+		configRoot.walkTopDown().forEach { file ->
+			val relativeFile = file.relativeTo(configRoot)
+			val destFile = Path.of(directory, "configs", appName, relativeFile.name).toFile()
+			val contents = file.readText()
+			val collapsed = osConfig.collapseVariables(contents)
+			destFile.writeText(collapsed)
+		}
+		 */
+		var appConfig = _config.Configs[appName];
+		var configRoot = new DirectoryInfo(appConfig.ConfigLocation);
+		TraverseTree(configRoot, appConfig.Ignore, appConfig.NoProcess,
+			(relativePath, file) =>
+			{
+				var collapsedText = _osConfig.CollapseVariables(file.OpenText().ReadToEnd());
+
+				// TODO: Write collapsed text to file in backup location (create dirs, if necessary)
+			},
+			(relativePath, file) =>
+			{
+				file.CopyTo(Path.Combine(_config.ConfigBackupDir, appName, relativePath));
+			});
 	}
 
 	public void Backup()
@@ -31,7 +56,19 @@ public class ConfigRepo
 
 	public void Restore(string appName)
 	{
+		/*
+		val appConfig = getConfig().configs[appName] ?: error("Invalid application name")
+		val configRoot = Path.of(directory, "configs", appName).toFile()
+		val osConfig = OsConfig()
 
+		configRoot.walkTopDown().forEach { file ->
+			val relativeFile = file.relativeTo(configRoot)
+			val destFile = Path.of(appConfig.configLocation, relativeFile.name).toFile()
+			val contents = file.readText()
+			val expanded = osConfig.expandVariables(contents)
+			destFile.writeText(expanded)
+		}
+		 */
 	}
 
 	public void Restore()
@@ -39,6 +76,44 @@ public class ConfigRepo
 		foreach (var (appName, _) in _config.Configs)
 		{
 			Restore(appName);
+		}
+	}
+
+	private static void TraverseTree(DirectoryInfo baseDir, List<string> ignoreList, List<string> noProcessList,
+		Action<string, FileInfo> process, Action<string, FileInfo> backupOnly)
+	{
+		var dirs = new Stack<DirectoryInfo>();
+		dirs.Push(baseDir);
+
+		while (dirs.Count > 0)
+		{
+			var currentDir = dirs.Pop();
+			var subDirs = currentDir.GetDirectories();
+			var files = currentDir.GetFiles();
+
+			foreach (var file in files)
+			{
+				var relativeName = file.FullName[baseDir.FullName.Length..].Replace("\\", "/");
+
+				if (!ignoreList.Contains(relativeName))
+				{
+					if (!noProcessList.Contains(relativeName))
+					{
+						// Process and backup
+						process(relativeName, file);
+					}
+					else
+					{
+						// Just backup
+						backupOnly(relativeName, file);
+					}
+				}
+			}
+
+			foreach (var dir in subDirs)
+			{
+				dirs.Push(dir);
+			}
 		}
 	}
 }
